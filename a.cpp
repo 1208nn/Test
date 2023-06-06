598422e988787359aa7504b566a4edee7d6381b0
1208nn <627871794@qq.com>
2023-06-06 16:08:49 +0800

    edit

--- a.cpp
+++ a.cpp
@@ -1,9 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int a;
-	cin>>a;
-	cout<<a;
-	cout<<"helloworld\n";
+	cout<<"\033[91mHello, world!\033[0m";
 	return 0;
 }
--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-print(1)
+print('\033[91m' + 'Hello, world!' + '\033[0m')
