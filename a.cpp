8f850cf7eaa8e1c76f0aa69d0543371bd0bb33fb
1208nn <627871794@qq.com>
2023-06-22 11:39:04 +0800

    1

--- a.cpp
+++ a.cpp
@@ -1,6 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-	cout<<"\033[91mHello, world!\033[0m";
+int main()
+{
+	cout << "\033[91mHello, world!\033[0m\n\n\n";
+	for (int i = 0; i < 128; i++)
+	{
+		cout << i << '\t' << char(i) << '\t';
+		if (!((i + 1) % 4))
+		{
+			cout << endl;
+		}
+	}
 	return 0;
 }
---
+++ webviewtest.py
@@ -0,0 +1,4 @@
+import webview
+
+webview.create_window("My Browser", "https://www.bing.com")
+webview.start()
